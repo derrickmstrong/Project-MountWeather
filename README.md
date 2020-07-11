4af6fb99d527145874c3618f9bdd5cfad8dca82c
Derrick Strong <derrickmstrong@gmail.com>
2020-07-11 07:00:51 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mount Weather
+## React Weather app
+
+### This app includes a basic React Hooks component with a specific weather API endpoint. 
+
+This app uses:
+- React Hooks
+- Bootstrap
+- Weather API
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ByCityId from './components/ByCityId';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ByCityId />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
